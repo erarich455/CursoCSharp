ae7378b48e8fd2b070c62de93a8d9cb6bc509b44
Erick Oliveira <dalek123d@gmail.com>
2023-03-21 01:04:46 -0300

    escrevendo write, string e write line

--- Program.cs
+++ Program.cs
@@ -1,9 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int i;
-int i2 = i = 10;
-
-i2--;
-int i3 = i2--;
-Console.WriteLine(i2--);
+﻿Console.Write("Digite seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine("Olá " + name);
