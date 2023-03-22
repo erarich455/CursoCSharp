f4e6a47ee90877fc2ac9b82d5eaae71b2f859da4
Erick Oliveira <dalek123d@gmail.com>
2023-03-22 00:23:32 -0300

    Aprendendo sobre int.parse entre outros

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿Console.Write("Digite seu nome: ");
 string name = Console.ReadLine();
-Console.WriteLine("Olá " + name);
+Console.WriteLine($"Olá {name}!");
+Console.Write("Digite o ano do seu nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2022 - year; //+, - ,* ,/
+Console.WriteLine($"Você tem {age} anos");
+
+if(age >= 18 || name =="Fredi") // "||" significa "ou"
+{
+    Console.WriteLine("Você é maior de idade"); // "if" seria um "E se"
+}
+else
+ {
+    Console.WriteLine("Você é menor de idade"); // "else" seria um "E se não", mas neste caso também poderia ser usado "if" no lugar de "else
+ }
