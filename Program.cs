6d9cfccb5d3ab175f4e0ab2eeae43e976b1e3fd0
Erick Oliveira <dalek123d@gmail.com>
2023-03-24 01:10:33 -0300

    strings

--- Program.cs
+++ Program.cs
@@ -1,16 +1,9 @@
-﻿Console.Write("Digite seu nome: ");
-string name = Console.ReadLine();
-Console.WriteLine($"Olá {name}!");
-Console.Write("Digite o ano do seu nascimento: ");
-int year = int.Parse(Console.ReadLine());
-int age = 2022 - year; //+, - ,* ,/
-Console.WriteLine($"Você tem {age} anos");
-
-if(age >= 18 || name =="Fredi") // "||" significa "ou"
+﻿class Program
 {
-    Console.WriteLine("Você é maior de idade"); // "if" seria um "E se"
+    static void Main() //método main é o primeiro método que vai ser executado pelo runtime do dotnet e ele PRECISA ser um método estatico
+{
+  string name = "Fredi";
+
+  name.
 }
-else
- {
-    Console.WriteLine("Você é menor de idade"); // "else" seria um "E se não", mas neste caso também poderia ser usado "if" no lugar de "else
- }
+}
