46d34654f908611d7c51f5339f1728d83d3406c0
Erick Oliveira <dalek123d@gmail.com>
2023-03-25 02:26:54 -0300

    são tantas coisas

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
 ﻿class Program
 {
     static void Main() //método main é o primeiro método que vai ser executado pelo runtime do dotnet e ele PRECISA ser um método estatico
-{
-  string name = "Fredi";
+  {
+    BankAccount account1 = new BankAccount();
+account1.name = null;
+account1.balance = -100;
+    BankAccount account2 = new BankAccount();
 
-  name.
+  }
 }
+
+class BankAccount
+{
+    private string name;//tipo e nome do campo, seu identificador
+    private decimal balance;
+
+    public BankAccount(string name, decimal balance)
+    {
+      if(string.IsNullOrWhiteSpace(name))
+      {
+        throw new ArgumentException("Nome inválido", "name");
+      }
+      if (balance < 0)
+      {
+        throw new ArgumentException("Saldo não pode ser negativo");
+      }
+      this.name = name;
+      this.balance = balance;
+    }
+
 }
